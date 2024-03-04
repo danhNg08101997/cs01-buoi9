e0d6aca869e96c5ab180a6aa7c1a938098de96e2
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-03-04 23:41:36 +0700

    update bai tap 2

--- bai_tap_2.py
+++ bai_tap_2.py
@@ -8,6 +8,8 @@ def two_sum (lst, target):
         for i in range(index + 1, len(lst)):
             if lst[i] == result:
                 output = [index, i]
+    if output == []:
+        output = f'không có giá trị phù hợp'
     return output
 # Output: [0,1] vì num[0] + num[1] = 2+7 = 9 ngược lại nếu không có
-print(two_sum(lst_number, 24))
+print(two_sum(lst_number, 14))
